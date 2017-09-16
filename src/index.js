9d6a4b57bd77a407c10f38f2bd6ed18d57262e09
Slegan <Sleganlegat@gmail.com>
2017-09-16 15:45:41 +0300

    fix console.log

--- src/index.js
+++ src/index.js
@@ -10,21 +10,15 @@ module.exports = function check(str, bracketsConfig) {
   const strAllowOpenTag = allowOpenTag.join('');
   let allowCloseTag = [];
   for (var i = 0; i < str.length; i++) {
-    console.log(str[i], i ,'str[i], i');
     if (allowCloseTag[allowCloseTag.length-1] === strAllowOpenTag[strAllowOpenTag.indexOf(str[i])]) {
-      console.log(allowCloseTag, 'allowCloseTag before obn');
       allowCloseTag.pop();
-      console.log(allowCloseTag, 'allowCloseTag after obn');
     }else if (strAllowOpenTag.indexOf(str[i]) % 2 === 0) {
       allowCloseTag.push(strAllowOpenTag[strAllowOpenTag.indexOf(str[i]) + 1]);
-      console.log(allowCloseTag, 'определили ожидаемый закрывающий тэг');
     }else if (allowCloseTag[allowCloseTag.length-1] !== strAllowOpenTag[strAllowOpenTag.indexOf(str[i])]) {
-      console.log(allowCloseTag, 'false');
       result = false;
     }
     if (str.length - 1 === i) {
       if (allowCloseTag.length !== 0) {
-        console.log(allowCloseTag, 'if false');
         result = false;
       }
     }
